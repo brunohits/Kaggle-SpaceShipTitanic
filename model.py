a9bbbbff1bdcdb334b282d21325020b1adcc515b
NikSuperPlay <NikSuperPlay123@rambler.ru>
2024-03-17 02:58:02 +0700

    Rewrote methods, worked with poetry

--- model.py
+++ model.py
@@ -5,12 +5,10 @@ import argparse
 import optuna
 import pandas as pd
 from catboost import CatBoostClassifier
-from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
-
+logging.basicConfig(filename='./data/log_file.log', level=logging.INFO)
 class My_Classifier_Model:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -18,152 +16,132 @@ class My_Classifier_Model:
 
     def train(self, dataset_filename):
         df = pd.read_csv(dataset_filename)
-        df = df.drop(["Cabin", "Name"], axis=1)
-        df["Total expenses"] = df["RoomService"] + df["FoodCourt"] + df["ShoppingMall"] + df["Spa"]
-        df["Total expenses"] = df["Total expenses"] + df["VRDeck"]
-        df = df.drop(["RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"], axis=1)
+        df.HomePlanet.value_counts(dropna=False)
+        df.CryoSleep.value_counts(dropna=False)
+        df.VIP.value_counts(dropna=False)
+        passenger_attributes = df.drop(columns=["Transported"])  # X
+        passenger_labels = df["Transported"]  # y
+        passenger_attributes.drop(columns=["Name", "Cabin", "PassengerId"], inplace=True)
+        passenger_attributes = pd.get_dummies(passenger_attributes, drop_first=True)
+        passenger_attributes = passenger_attributes.dropna()
+        passenger_labels = passenger_labels[passenger_attributes.index]
 
-        x = df["Total expenses"].mean()
-        mask1 = df["Total expenses"] >= x
-        df.loc[mask1, "Total expenses"] = 1
-        mask2 = df["Total expenses"] != 1
-        df.loc[mask2, "Total expenses"] = 0
-        df = df.dropna()
-        df = df.reset_index()
-        df = df.drop("index", axis=1)
+        # Initialize Logistic Regression models
+        model = LogisticRegression()
 
-        df["Transported"] = df["Transported"].astype("int")
-        df["CryoSleep"] = df["CryoSleep"].astype("int")
-        df["VIP"] = df["VIP"].astype("int")
-        df["Total expenses"] = df["Total expenses"].astype("int")
-        df["HomePlanet"].value_counts()
+        # Train the models
+        model.fit(passenger_attributes, passenger_labels)
 
-        HomePlanet_is_Earth = df["HomePlanet"] == "Earth"
-        HomePlanet_is_Europa = df["HomePlanet"] == "Europa"
-        HomePlanet_is_Mars = df["HomePlanet"] == "Mars"
-        df.loc[HomePlanet_is_Earth, "HomePlanet"] = 0
-        df.loc[HomePlanet_is_Europa, "HomePlanet"] = 1
-        df.loc[HomePlanet_is_Mars, "HomePlanet"] = 2
+        # Evaluate model performance
+        score = model.score(passenger_attributes, passenger_labels)  # score on test set
+        # Print scores
+        print("Score :", score)
 
-        df["Destination"].value_counts()
-        Destination_is_TRAPPIST = df["Destination"] == "TRAPPIST-1e"
-        Destination_is_Cancri = df["Destination"] == "55 Cancri e"
-        Destination_is_PSO = df["Destination"] == "PSO J318.5-22"
-        df.loc[Destination_is_TRAPPIST, "Destination"] = 0
-        df.loc[Destination_is_Cancri, "Destination"] = 1
-        df.loc[Destination_is_PSO, "Destination"] = 2
-        df["HomePlanet"] = df["HomePlanet"].astype("int")
-        df["Destination"] = df["Destination"].astype("int")
-        df["Age"] = df["Age"].astype("int")
-
-        y = df["Transported"]
-        x = df.drop(["Transported", "PassengerId"], axis=1)
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
-        self.model = LogisticRegression(random_state=0, max_iter=1000)
-        self.model.fit(X_train, y_train)
-
+        # Save the trained models
         model_dir = './model'
         os.makedirs(model_dir, exist_ok=True)
         model_filepath = os.path.join(model_dir, 'model.pkl')
         with open(model_filepath, 'wb') as f:
-            pickle.dump(self.model, f)
+            pickle.dump(model, f)
 
         self.logger.info('Model training completed.')
-        self.logger.info(f'Training accuracy: {self.model.score(X_train, y_train)}')
-        self.logger.info(f'Test accuracy: {self.model.score(X_test, y_test)}')
 
     def predict(self, dataset_filename, model_name):
+        self.logger.info('Prediction started.')
         if model_name == 'model':
-            model_filename = "model/model.pkl"
+            model_filepath = "model/model.pkl"
         elif model_name == 'tuned_model':
-            model_filename = "model/tuned_model.pkl"
-        with open(model_filename, 'rb') as f:
+            model_filepath = "model/tuned_model.pkl"
+
+        with open(model_filepath, 'rb') as f:
             model = pickle.load(f)
 
-        df = pd.read_csv(dataset_filename)
-        test_ids = df["PassengerId"]
+        df_test = pd.read_csv(dataset_filename)
+        df_test = df_test.set_index("PassengerId")
+        df_test.drop(columns=["Name", "Cabin"], inplace=True)
+        df_test = pd.get_dummies(df_test, drop_first=True)
+        df_test = df_test.fillna(0)
 
-        df = df.drop(["Cabin", "Name", "PassengerId"], axis=1) if model_name == 'model' else df.drop(["Cabin", "Name"], axis=1)
-        df["Total expenses"] = df["RoomService"] + df["FoodCourt"] + df["ShoppingMall"] + df["Spa"]
-        df["Total expenses"] = df["Total expenses"] + df["VRDeck"]
-        df = df.drop(["RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"], axis=1)
-        df["HomePlanet"] = df["HomePlanet"].map({"Earth": 0, "Europa": 1, "Mars": 2})
-        df["Destination"] = df["Destination"].map({"TRAPPIST-1e": 0, "55 Cancri e": 1, "PSO J318.5-22": 2})
+        # Load train data to get true labels
+        df_train = pd.read_csv("data/train.csv")
+        df_train = df_train.set_index("PassengerId")
+        true_labels = df_train["Transported"]
 
-        imputer = SimpleImputer(strategy='mean')
-        df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+        predictions = model.predict(df_test)
+        # accuracy = accuracy_score(true_labels, predictions)
+        # print("Accuracy:", accuracy)  Uncomment to get accurancy
 
-        submission_pred = model.predict(df)
+        df_test["Transported"] = predictions
+        submission = df_test[["Transported"]]
 
-        dfinal = pd.DataFrame({"PassengerId": test_ids.values, "Transported": submission_pred})
-        dfinal["Transported"] = dfinal["Transported"].astype("bool")
-
+        # Save submission.csv in the data directory
         results_dir = './data'
         os.makedirs(results_dir, exist_ok=True)
-        results_filepath = os.path.join(results_dir, 'results.csv')
-        dfinal.to_csv(results_filepath, index=False)
+        result_filepath = os.path.join(results_dir, 'result.csv')
+        submission.to_csv(result_filepath)
 
-        self.logger.info(f'Прогноз сохранен в {results_filepath}')
+        self.logger.info(f'Prediction saved in ./data/result.csv')
 
+    def tune_hyperparameters(self, dataset_filename):
+        self.logger.info('Hyperparameter tuning started.')
+        df = pd.read_csv(dataset_filename)
+        df.HomePlanet.value_counts(dropna=False)
+        df.CryoSleep.value_counts(dropna=False)
+        df.VIP.value_counts(dropna=False)
+        passenger_attributes = df.drop(columns=["Transported"])  # X
+        passenger_labels = df["Transported"]  # y
+        passenger_attributes.drop(columns=["Name", "Cabin", "PassengerId"], inplace=True)
+        passenger_attributes = pd.get_dummies(passenger_attributes, drop_first=True)
+        passenger_attributes = passenger_attributes.dropna()
+        passenger_labels = passenger_labels[passenger_attributes.index]
 
-def tune_hyperparameters(dataset_filename):
-    df = pd.read_csv(dataset_filename)
+        def objective(trial):
+            # Define space of hyperparameters to search
+            params = {
+                'depth': trial.suggest_int('depth', 4, 10),
+                'learning_rate': trial.suggest_uniform('learning_rate', 0.01, 0.5),
+                'iterations': trial.suggest_int('iterations', 100, 1000),
+                'l2_leaf_reg': trial.suggest_loguniform('l2_leaf_reg', 1e-8, 100)
+            }
 
-    # Подготовка данных для обучения
-    df = df.drop(["Cabin", "Name", "PassengerId"], axis=1)
-    df["Total expenses"] = df["RoomService"] + df["FoodCourt"] + df["ShoppingMall"] + df["Spa"]
-    df["Total expenses"] = df["Total expenses"] + df["VRDeck"]
-    df = df.drop(["RoomService", "FoodCourt", "ShoppingMall", "Spa", "VRDeck"], axis=1)
-    df["HomePlanet"] = df["HomePlanet"].map({"Earth": 0, "Europa": 1, "Mars": 2})
-    df["Destination"] = df["Destination"].map({"TRAPPIST-1e": 0, "55 Cancri e": 1, "PSO J318.5-22": 2})
+            # Initialize CatBoostClassifier model
+            model = CatBoostClassifier(**params)
 
-    imputer = SimpleImputer(strategy='mean')
-    df = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
+            # Split dataset
+            X_train, X_valid, y_train, y_valid = train_test_split(passenger_attributes, passenger_labels, test_size=0.2)
 
-    X = df.drop(["Transported"], axis=1)
-    y = df["Transported"]
+            # Train the model
+            model.fit(X_train, y_train, eval_set=(X_valid, y_valid), verbose=False)
 
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+            # Evaluate model
+            score = model.score(X_valid, y_valid)
 
-    def objective(trial):
-        params = {
-            "C": trial.suggest_loguniform("C", 0.001, 1000),
-            "max_iter": trial.suggest_int("max_iter", 100, 1000),
-        }
-        model = LogisticRegression(random_state=0, **params)
+            return score
 
-        model.fit(X_train, y_train)
+        study = optuna.create_study(direction="maximize")
+        study.optimize(objective, n_trials=8)
 
-        pred = model.predict(X_val)
+        best_params = study.best_params
 
-        accuracy = accuracy_score(y_val, pred)
+        # Initialize and train the model with the best hyperparameters
+        tuned_model = CatBoostClassifier(**best_params)
+        tuned_model.fit(passenger_attributes, passenger_labels, verbose=False)
 
-        return accuracy
+        # Save the trained model
+        model_dir = './model'
+        os.makedirs(model_dir, exist_ok=True)
+        model_filepath = os.path.join(model_dir, 'tuned_model.pkl')
+        with open(model_filepath, 'wb') as f:
+            pickle.dump(tuned_model, f)
 
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=100)
-
-    best_params = study.best_trial.params
-
-    model = LogisticRegression(random_state=0, **best_params)
-    model.fit(X_train, y_train)
-
-    model_dir = './model'
-    os.makedirs(model_dir, exist_ok=True)
-    model_filepath = os.path.join(model_dir, 'tuned_model.pkl')
-    with open(model_filepath, 'wb') as f:
-        pickle.dump(model, f)
-
-    print(f"Best hyperparameters found: {best_params}")
-    print(f"Model saved to {model_filepath}")
+        logging.info('Hyperparameter tuning completed.')
 
 
 def main():
     parser = argparse.ArgumentParser(description='Model training and prediction')
-    parser.add_argument('mode', choices=['train', 'predict','tune'], help='Mode: train, tune or predict')
+    parser.add_argument('mode', choices=['train', 'predict', 'tune'], help='Mode: train, tune or predict')
     parser.add_argument('--dataset', required=True, help='Path to dataset file')
-    parser.add_argument('--model',choices=['tuned_model','model'], required=True, help='Name of the model')
+    parser.add_argument('--model', choices=['tuned_model', 'model'], required=False, help='Name of the model')
     args = parser.parse_args()
 
     model = My_Classifier_Model()
@@ -173,7 +151,7 @@ def main():
     elif args.mode == 'predict':
         model.predict(args.dataset, args.model)
     elif args.mode == 'tune':
-        tune_hyperparameters(args.dataset)
+        model.tune_hyperparameters(args.dataset)
 
 
 if __name__ == '__main__':
